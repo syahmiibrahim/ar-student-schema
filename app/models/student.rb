acb401efc04d360adf7c580e41faa02c546652c7
syahmiibrahim <syahmiibrahim@live.com>
2015-08-19 16:43:16 +0800

    msg

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,24 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, uniqueness: true, format: { with: /\A\w+@\w+(.\w+)*(\.\w{2,})\z/ }
+  validates :age, numericality: { greater_than: 4}
+  validate :phone_validation?
+
+  attr_accessor :birthday
+
+  def name
+	"#{first_name} #{last_name}"
+  end
+
+  def age
+	now = Date.today
+	age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day))? 0:1)
+  end
+
+  def phone_validation?
+  	if self.phone.scan(/\d+/).join.length < 10
+  		errors.add(:phone, "Should contain at least 10 digits")
+  	end
+  end
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,14 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |s|
+    	s.string :first_name
+    	s.string :last_name
+    	s.string :gender
+    	s.string :email
+    	s.string :phone
+    	s.timestamps null: false
+    end
   end
 end
+# endrake 
